9825d52460fe47fa8f52ecd844052896ec8fca07
Grace Tveit <grace.tveit@gmail.com>
2022-12-13 17:13:39 -0800

    feat: solve 1.2

--- src/calorie_counting.rs
+++ src/calorie_counting.rs
@@ -36,6 +36,30 @@ pub fn second_solution(calorie_list: String) -> i32 {
     largest_sum
 }
 
+pub fn part_two(calorie_list: String) -> i32 {
+    let mut largest_sums = [0, 0, 0];
+    let mut current_sum = 0;
+
+    for x in calorie_list.split("\n") {
+        if x == "" {
+            replace_lowest_item(&mut largest_sums, current_sum);
+            current_sum = 0;
+        } else {
+            current_sum += x.parse::<i32>().unwrap()
+        }
+    }
+    replace_lowest_item(&mut largest_sums, current_sum);
+    largest_sums.into_iter().sum()
+}
+
+fn replace_lowest_item(sum_list: &mut [i32; 3], x: i32) {
+    sum_list.sort();
+    // find the lowest item, and get index
+    if sum_list[0] < x {
+        sum_list[0] = x
+    }
+}
+
 #[cfg(test)]
 fn test_string() -> String {
     String::from(
@@ -66,4 +90,10 @@ fn test_parse_string() {
 fn test_calorie_counting() {
     let test_string = test_string();
     assert_eq!(calorie_counting(test_string), 24000)
+}
+
+#[test]
+fn test_second_part() {
+    let test_string = test_string();
+    assert_eq!(part_two(test_string), 45000)
 }
